215e9907cc2492f6ecc16f10d1ff9dfbdb5b90ab
TheHitMan7 <krtik.vrma@gmail.com>
2021-06-19 10:37:40 +0530

    ginkgo: Update thermal config
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- device.mk
+++ device.mk
@@ -418,9 +418,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
-    $(LOCAL_PATH)/configs/thermal/thermal-engine-camera.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-camera.conf \
-    $(LOCAL_PATH)/configs/thermal/thermal-engine-map.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-map.conf \
-    $(LOCAL_PATH)/configs/thermal/thermal-engine-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-normal.conf
+    $(LOCAL_PATH)/configs/thermal/thermal-engine-map.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-map.conf
 
 # USB
 PRODUCT_PACKAGES += \
